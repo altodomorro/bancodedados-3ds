cebf1ec88d1eed5472bae570f3608e32103992ed
altodomorro <lino.vinicius@escola.pr.gov.br>
2026-03-26 07:55:48 -0300

    Create LIVROS table and insert initial data
    Created a table for books with initial data entries.

---
+++ livraria.sql
@@ -0,0 +1,20 @@
+-- 1️⃣ Criar a tabela LIVROS
+CREATE TABLE LIVROS (
+    ID_LIVRO INT PRIMARY KEY,
+    NOME_LIVRO VARCHAR(100),
+    AUTORIA VARCHAR(100),
+    CATEGORIA VARCHAR(50),
+    EDITORA VARCHAR(50),
+    PREÇO DECIMAL(10,2)
+);
+
+-- 2️⃣ Inserir os dados
+INSERT INTO LIVROS 
+(CATEGORIA, AUTORIA, NOME_LIVRO, EDITORA, ID_LIVRO, PREÇO)
+VALUES
+('Biografia', 'Malala Yousafzai', 'Eu sou Malala', 'Companhia das Letras', 11, 22.32),
+('Biografia', 'Michelle Obama', 'Minha história', 'Objetiva', 12, 57.90),
+('Biografia', 'Anne Frank', 'Diário de Anne Frank', 'Pe Da Letra', 13, 34.90);
+
+-- 3️⃣ Exibir todos os registros da tabela LIVROS
+SELECT * FROM LIVROS;
